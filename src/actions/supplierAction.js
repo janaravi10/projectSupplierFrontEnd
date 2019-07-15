9c318a6146bd6552db6a2426d49380220a1a0734
janaravi10 <janaravi962@gmail.com>
2019-07-15 17:24:08 +0530

    Some small code tweak

--- src/actions/supplierAction.js
+++ src/actions/supplierAction.js
@@ -10,14 +10,14 @@ export function submitSupplierData(supplierData, isEdit) {
   return dispatch => {
     let url = "http://localhost:5000/supplier/";
     url += isEdit ? "edit" : "add";
-    console.log(url)
+    console.log(url);
     axios
       .post(url, {
         supplier: supplierData
       })
       .then(res => {
-        console.log(res)
-        
+        console.log(res);
+
         if (res.data.type === "SUCCESSFUL") {
           dispatch({
             type: FORM_SUBMITTED,
@@ -46,7 +46,7 @@ export function getListOfSupplier() {
     axios
       .get("http://localhost:5000/supplier/get")
       .then(res => {
-        if (res.data.type === "SUCCESS") {
+        if (res.data.type === "SUCCESSFUL") {
           dispatch({
             type: GET_SUPPLIER,
             payload: { suppliers: res.data.response }
@@ -67,5 +67,22 @@ export function closeSnackBar(closeBarState, bar) {
         formSubmitSuccess: closeBarState
       }
     });
+  };
+}
+
+// clear notification
+export function deleteSupplier(supplierId) {
+  return dispatch => {
+    axios
+      .post("http://localhost:5000/supplier/delete", { supplierId })
+      .then(res => {
+        if (res.data.type === "SUCCESSFUL") {
+          console.log(res)
+          getListOfSupplier()(dispatch);
+        } else {
+          console.log(res)
+        }
+      })
+      .catch(function(err) {});
   };
 }
